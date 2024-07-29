1d8a9983a79a05ed27c089273875f6b35537e12c
Xingwang Yu <kevin.xwyu@gmail.com>
2024-07-29 15:13:04 -0400

    message

--- API.R
+++ API.R
@@ -1,9 +1,16 @@
+library(plumber)
+library(tidyverse)
+library(tidyr)
+library(caret)
+library(rpart)
+
+
 # read in data
 df <- read.csv("diabetes_binary_health_indicators_BRFSS2015.csv")
 
 # select variables 
 diab_df <- df |>
-  select(Diabetes_binary, HighBP, HighChol, Smoker, BMI, Veggies, HvyAlcoholConsump, DiffWalk, Sex, Age) |>
+  select(Diabetes_binary, HighBP, HighChol, Smoker, BMI, Sex, Age) |>
   mutate(Diabetes_binary = factor(Diabetes_binary,
                                   levels = c(0, 1),
                                   labels = c("no_diabetes", "prediabetes"))) |>
@@ -30,5 +37,44 @@ diab_df <- df |>
 ct_model <- rpart(Diabetes_binary ~ ., 
                   data = diab_df, 
                   cp = 0.011)
+
+# API function for pred endpoint
+#* @get /pred
+#* @param HighBP high blood pressure 0=no, 1=yes
+#* @param HighChol high cholesterol 0=no, 1=yes
+#* @param Smoker  0=no, 1=yes
+#* @param BMI 
+#* @param Sex  0=female, 1=male
+#* @param Age 
+
+function(HighBP=0, HighChol=0, BMI=28.4, Smoker=0, Sex=0, Age = 9) {
+  # Create a new data frame with the input values
+  new_data <- data.frame(
+    HighBP = as.factor(HighBP),
+    HighChol = as.factor(HighChol),
+    BMI = BMI,
+    Smoker = as.factor(Smoker),
+    Sex = as.factor(Sex),
+    Age = as.factor(Age),
+  )
+  
+  # Make prediction
+  prediction <- predict(model, new_data, type = "prob")
+  
+  # Convert prediction to class labels
+  predicted_class <- ifelse(prediction > 0.5, "prediabetes", "no_diabetes")
+  
+  # Return the predicted class
+  return(list(predicted_class = predicted_class))
+}
+
+
+# API function for info endpoint
+
+#* @get /info
+function(){
+  "Xingwang Yu"
+  "https://kevinyu86.github.io/ST558_final/"
+}
 
 
